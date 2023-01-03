422fe5782c135ff85791c9ddf47dd7cd59c29ef1
Taishi-Ito <taishi.ito1026@gmail.com>
2023-01-03 11:55:38 +0900

    Incorporated AppVideo into the top page

--- youtube-manager-nuxt/pages/index.vue
+++ youtube-manager-nuxt/pages/index.vue
@@ -1,18 +1,38 @@
 <template>
   <section class="section">
-    <div class="container"></div>
+    <div class="container">
+      <div class="block video-block" v-for="item in items">
+        <AppVideo :item="item" :videoId="item.id" />
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-import ROUTES from '~/routes/api'
-export default{
-  async fetch({store}) {
-    const payload = {
-      uri: ROUTES.GET.POPULARS
-    }
+  import ROUTES from '~/routes/api'
+  export default{
+    computed: {
+      items() {
+        return this.$store.getters.getPopularVideos
+      }
+    },
 
-    await store.dispatch('fetchPopularVideos', payload)
+    async fetch({store}) {
+      const payload = {
+        uri: ROUTES.GET.POPULARS
+      }
+
+      if (store.getters.getPopularVideos && store.getters.getPopularVideos.length > 0) {
+        return
+      }
+
+      await store.dispatch('fetchPopularVideos', payload)
+    }
   }
-}
 </script>
+
+<style scoped>
+  .video-block {
+    max-width: 900px;
+  }
+</style>
