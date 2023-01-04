c8ad178235cb399e57a760b3572a4731fa099fb9
Taishi-Ito <taishi.ito1026@gmail.com>
2023-01-04 10:39:00 +0900

    Changed FetchMostPopularVideos method to get more page

--- web/api/fetch_most_popular_videos.go
+++ web/api/fetch_most_popular_videos.go
@@ -19,6 +19,10 @@ func FetchMostPopularVideos() echo.HandlerFunc {
 			logrus.Fatalf("Error creating new Youtube service: %v", err)
 		}
 		call := yts.Videos.List([]string{"id", "snippet"}).Chart("mostPopular").MaxResults(3)
+		pageToken := c.QueryParam("pageToken")
+		if len(pageToken) > 0 {
+			call = call.PageToken(pageToken)
+		}
 		res, err := call.Do()
 		if err != nil {
 			logrus.Fatalf("Error calling Youtube API: %v", err)
